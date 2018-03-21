645355d8c887a4dddf611109ecac52533ca66d43
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-21 19:04:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,3 +39,18 @@ def winner(board)
     return board[won?(board)[0]]
   end
 end
+
+def turn_count(board)
+  count = 0
+  board.each do |place|
+    if place == "X" || place == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
